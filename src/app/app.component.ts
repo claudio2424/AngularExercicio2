f3817c37241d5955e6d3290b55c1fa1e26134381
Claudio Filho <claudiofilho025@gmail.com>
2022-03-08 21:55:21 -0300

    Segundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'projeto2';
+  title = 'projeto2'
+  tipoDoInput: string = 'text'
+  texto: string = 'Digite algo'
+  obrigatorio: boolean = true
+
+  valorInput: number = 0
+
+  constructor(
+    private _snackBar: MatSnackBar) {}
+
+  mostrarSnackBar():void{
+    this._snackBar.open('Você clicou neste botão :)', 'Ok')
+  }
+
+  alterarValor(event:any):void {
+    this.valorInput = event.target.value
+  }
+
+  decrementa(event:any){
+    this.valorInput--;
+  }
+  incrementa(event:any){
+    this.valorInput++;
+  }
 }
+
+
